be4bf6918b7f86012a36c76ee3dd9c2f64f10c3c
Deepak Narkhede <mailtodeepakn@gmail.com>
2016-12-01 06:09:55 +0530

    Database to HDFS Sync App: Add polling mechanism for Applcation Test for result records. (#27)

--- app-templates/database-to-hdfs/src/test/java/com/datatorrent/apps/ApplicationTest.java
+++ app-templates/database-to-hdfs/src/test/java/com/datatorrent/apps/ApplicationTest.java
@@ -24,15 +24,13 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Collection;
 
 import javax.validation.ConstraintViolationException;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.hadoop.conf.Configuration;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -40,6 +38,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
 
 import com.datatorrent.api.LocalMode;
 import com.datatorrent.apps.Application;
@@ -49,6 +52,7 @@ public class ApplicationTest
   private static final String DB_DRIVER = "org.postgresql.Driver";
   private static final String URL = "jdbc:postgresql://localhost:5432/testdb?user=postgres&password=postgres";
   private static final String TABLE_NAME = "test_event_table";
+  private static final Logger LOG = LoggerFactory.getLogger(ApplicationTest.class);
 
   private String outputDir;
 
@@ -89,7 +93,6 @@ public class ApplicationTest
   {
     try {
       Class.forName(DB_DRIVER).newInstance();
-
       Connection con = DriverManager.getConnection(URL);
       Statement stmt = con.createStatement();
 
@@ -132,32 +135,108 @@ public class ApplicationTest
     }
   }
 
+  private LocalMode.Controller asyncRun(Configuration conf) throws Exception
+  {
+    LocalMode lma = LocalMode.newInstance();
+    lma.prepareDAG(new Application(), conf);
+    LocalMode.Controller lc = lma.getController();
+    lc.runAsync();
+    return lc;
+  }
+
   @Test
   public void testApplication() throws Exception
   {
     try {
-      LocalMode lma = LocalMode.newInstance();
       Configuration conf = new Configuration(false);
       conf.addResource(this.getClass().getResourceAsStream("/META-INF/properties-test.xml"));
       conf.set("dt.operator.fileOutput.prop.filePath", outputDir);
 
-      lma.prepareDAG(new Application(), conf);
-      LocalMode.Controller lc = lma.getController();
-      lc.runAsync();
+      /*
+       * Run the application asynchronously and keep polling for results till timeout.
+       */
+      LocalMode.Controller lc = asyncRun(conf);
+      waitForOutputTuples();
 
-      // wait for output files to roll      
-      Thread.sleep(5000);
-
-      String[] extensions = { "dat.0", "tmp" };
-      Collection<File> list = FileUtils.listFiles(new File(outputDir), extensions, false);
-      int recordsCount = 0;
-      for (File file : list) {
-        recordsCount += FileUtils.readLines(file).size();
-      }
-      Assert.assertEquals("Records in file", 10, recordsCount);
-
+      cleanTable();
+      lc.shutdown();
     } catch (ConstraintViolationException e) {
       Assert.fail("constraint violations: " + e.getConstraintViolations());
+    }
+  }
+
+  private int getNumberOfTuplesProcessed()
+  {
+    String[] extensions = { "dat.0", "tmp" };
+    Collection<File> list = FileUtils.listFiles(new File(outputDir), extensions, false);
+    int recordsCount = 0;
+    for (File file : list) {
+      try {
+        recordsCount += FileUtils.readLines(file).size();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return recordsCount;
+  }
+
+  private void waitForOutputTuples() throws Exception
+  {
+    final int MAX_TIMEOUT = 60;
+    final int numOfInputTuples = getNumOfEventsInStore(TABLE_NAME);
+    int numOfOutputTuples = 0;
+
+    /*
+     * Wait till output directory is created.
+     */
+    File directory = (new File(outputDir));
+    for (int i = 0; i < MAX_TIMEOUT; i++) {
+      if (!directory.exists()) {
+        LOG.debug("Waiting for output directory {} to be created.", directory.getName());
+        Thread.sleep(1000);
+      } else {
+        LOG.debug("Output directory {} is created.", directory.getName());
+        break;
+      }
+    }
+
+    /*
+     * Periodically check for number of records processed in a file.
+     */
+    for (int i = 0; i < MAX_TIMEOUT; i++) {
+      numOfOutputTuples = getNumberOfTuplesProcessed();
+      if (numOfInputTuples != numOfOutputTuples) {
+        LOG.debug("Sleeping, Iteration({}): Records Found {} Total records {}", i, numOfOutputTuples, numOfInputTuples);
+        Thread.sleep(1000);
+      } else {
+        LOG.debug("Total Records Found {} Total Expected Records {}", numOfOutputTuples, numOfInputTuples);
+        break;
+      }
+    }
+
+    /*
+     * Verification of number of records processed in a file.
+     */
+    if (numOfInputTuples != numOfOutputTuples) {
+      String msg = String.format("Error: Records in file expected %d found %d after %d seconds%n", numOfInputTuples, numOfOutputTuples, MAX_TIMEOUT);
+      Assert.assertEquals("Records in file", numOfInputTuples, numOfOutputTuples);
+      throw new RuntimeException(msg);
+    }
+  }
+
+  public int getNumOfEventsInStore(String tableName)
+  {
+    Connection con;
+    try {
+      con = DriverManager.getConnection(URL);
+      Statement stmt = con.createStatement();
+
+      String countQuery = "SELECT count(*) from " + tableName;
+      ResultSet resultSet = stmt.executeQuery(countQuery);
+      resultSet.next();
+      return resultSet.getInt(1);
+    } catch (SQLException e) {
+      throw new RuntimeException("fetching count", e);
     }
   }
 }
